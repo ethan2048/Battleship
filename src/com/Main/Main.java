1d53b92ef9d73317f89d1689dc675ab5f615ae02
ethan dobski <xenox2048@gmail.com>
2014-05-07 01:07:55 +0400

    First commit

---
+++ src/com/Main/Main.java
@@ -0,0 +1,25 @@
+package com.Main;
+
+import com.battleship.Field;
+
+import Methods.*;
+
+
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		int LENGHT = 4;
+		int SHIP_LENGHT = 4;
+		
+		Field bfield = new Field(LENGHT);
+		KeyboardInput kb = new KeyboardInput();
+		boolean indent = kb.arrayTest(bfield.getFieldVector(0), 1, LENGHT, SHIP_LENGHT);
+		System.out.println(indent);
+		
+		
+		
+	}
+
+}
---
+++ src/com/battleship/Field.java
@@ -0,0 +1,51 @@
+package com.battleship;
+
+public class Field {
+	
+	private final int LINE_SIZE;
+	
+	private char[][] field;
+	
+	public Field() {
+		LINE_SIZE = 10;
+		field = new char[LINE_SIZE][LINE_SIZE];
+	}
+	
+	public Field(int LINE_SIZE) {
+		this.LINE_SIZE = LINE_SIZE;
+		this.field = new char[LINE_SIZE][LINE_SIZE];
+	}
+
+	public char[][] getField() {
+		return field;
+	}
+
+	public void setField(char[][] field) {
+		this.field = field;
+	}
+	
+	public char getFieldCell(int y, int x) { //возвращает значение клетки поля, либо '0' если невозможно получить
+		if ((x<this.LINE_SIZE) && (y<this.LINE_SIZE) && (x>-1) && (y>-1)) return this.field[y][x];
+		else return '0';
+	}
+	
+	public void setFieldCell(int y, int x, char character) { //сеттер для клетки
+		if ((x<this.LINE_SIZE) && (y<this.LINE_SIZE) && (x>-1) && (y>-1)) this.field[y][x] = character;
+	}
+		
+	public int getLINE_SIZE() {
+		return LINE_SIZE;
+	}
+	
+	public char[] getFieldVector(int y) {
+		return field[y]; 
+	}
+	
+	public void fieldView() {
+		for (int i = 0; i < LINE_SIZE; i++) {
+			Methods.Array.viewCharArray(this.field[i], this.LINE_SIZE);
+		}
+	}
+	
+	
+}
